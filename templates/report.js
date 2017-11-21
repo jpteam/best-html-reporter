f57e0674679afdfcb77b7c234508592d3aff97f9
nimmi <nimmisundarraj@gmail.com>
2017-11-22 00:22:10 +0530

    reduce functions

--- templates/report.js
+++ templates/report.js
@@ -3,26 +3,11 @@ angular.module('app', [])
     $scope.results = results;
     $scope.jsonData = JSON.stringify(results, null, 4);
     $scope.suite = results.suites.length;
+    $scope.data = results.suites.reduce(function(suiteTot, suite) {
+      var specCount = suite.specs.reduce(function(specTot, spec) {
+        return {fail: specTot.fail + spec.failedExpectations.length, pass: specTot.pass + spec.passedExpectations.length};
+      }, {fail:0, pass:0})
 
-    function getTestCases(){
-    $scope.count = 0;
-      for (var index = 0; index < $scope.suite; index++) {
-       $scope.count += results.suites[index].specs.length;
-      }
-      return $scope.count;
-    }
-
-    $scope.testCase = getTestCases();
-
-    function getExpectation() {
-    $scope.count = 0;
-      for (var suite = 0; suite < $scope.suite; suite++) {
-        for (var spec = 0; spec < $scope.testCase; spec++) {
-          $scope.count += results.suites[suite].specs[spec].failedExpectations.length + results.suites[suite].specs[spec].passedExpectations.length;
-        }
-      }
-      return $scope.count;
-    }
-
-    $scope.expectations = getExpectation();
+      return {specs: suite.specs.length, fail: suiteTot.fail + specCount.fail, pass: suiteTot.pass + specCount.pass};
+    }, {test:0, fail:0, pass:0});
   });
