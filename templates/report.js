823c02cd6015e23d54d25679dea57a7d85c86311
nimmi <nimmisundarraj@gmail.com>
2017-11-24 00:35:46 +0530

    added total test pass functionality

--- templates/report.js
+++ templates/report.js
@@ -3,11 +3,18 @@ angular.module('app', [])
     $scope.results = results;
     $scope.jsonData = JSON.stringify(results, null, 4);
     $scope.suite = results.suites.length;
-    $scope.data = results.suites.reduce(function(suiteTot, suite) {
-      var specCount = suite.specs.reduce(function(specTot, spec) {
-        return {fail: specTot.fail + spec.failedExpectations.length, pass: specTot.pass + spec.passedExpectations.length};
-      }, {fail:0, pass:0})
 
-      return {specs: suite.specs.length, fail: suiteTot.fail + specCount.fail, pass: suiteTot.pass + specCount.pass};
-    }, {test:0, fail:0, pass:0});
+    $scope.expectations = results.suites.reduce(function(suiteTot, suite) {
+
+      suiteTot.totalTest += suite.specs.length;
+
+      var specCount = suite.specs.reduce(function(specTot, spec) {
+
+         specTot.testPass += spec.status == "passed" ? 1 : 0;
+
+        return {testPass: specTot.testPass, fail: specTot.fail + spec.failedExpectations.length, pass: specTot.pass + spec.passedExpectations.length};
+      }, {testPass:0, fail:0, pass:0})
+
+      return {testPass: suiteTot.testPass + specCount.testPass, totalTest: suiteTot.totalTest, fail: suiteTot.fail + specCount.fail, pass: suiteTot.pass + specCount.pass};
+    }, {testPass:0, totalTest:0, fail:0, pass:0});
   });
