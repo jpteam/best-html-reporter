63b6927ab3bc745e3625ad296fd858e8b225aa51
rmadhuram <raj@opsclarity.com>
2017-11-24 02:30:48 -0800

    fix for hierarchical representation of suites.

--- templates/report.js
+++ templates/report.js
@@ -2,12 +2,43 @@ angular.module('app', [])
   .controller('ReportController', function($scope, $locale) {
     $scope.results = results;
     $scope.jsonData = JSON.stringify(results, null, 4);
-    $scope.suite = results.suites.length;
-    $scope.data = results.suites.reduce(function(suiteTot, suite) {
-      var specCount = suite.specs.reduce(function(specTot, spec) {
-        return {fail: specTot.fail + spec.failedExpectations.length, pass: specTot.pass + spec.passedExpectations.length};
-      }, {fail:0, pass:0})
 
-      return {specs: suite.specs.length, fail: suiteTot.fail + specCount.fail, pass: suiteTot.pass + specCount.pass};
-    }, {test:0, fail:0, pass:0});
+    var metrics = {
+      totalSuites: results.suites.length,
+      passedSuites: 0,
+      totalSpecs: 0,
+      passedSpecs: 0,
+      totalExp: 0,
+      passedExp: 0
+    };
+
+    for (var i = 0; i < results.suites.length; i++) {
+      var thisSuite = results.suites[i],
+        numFailedSpecs = 0, // failed specs in this suite.
+        numPassedExp = 0;
+
+      for (var j = 0; j < thisSuite.specs.length; j++) {
+        var thisSpec = thisSuite.specs[j],
+          failedExpectations = thisSpec.failedExpectations,
+          passedExpectations = thisSpec.passedExpectations;
+
+        if (failedExpectations.length > 0) {
+          numFailedSpecs ++;
+        }
+
+        numPassedExp += passedExpectations.length;
+
+        metrics.totalExp += (passedExpectations.length + failedExpectations.length);
+        metrics.passedExp += passedExpectations.length;
+      }
+
+      metrics.totalSpecs += thisSuite.specs.length;
+      metrics.passedSpecs += (thisSuite.specs.length - numFailedSpecs);
+
+      if (numFailedSpecs == 0) {
+        metrics.passedSuites ++;
+      }
+    }
+
+    $scope.metrics = metrics;
   });
