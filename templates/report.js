4b7f6d27c7fba4e9451785ced727ebb93a7b6665
rmadhuram <raj@opsclarity.com>
2017-11-26 01:07:56 -0800

    support for hierarchical data, progress visualization

--- templates/report.js
+++ templates/report.js
@@ -3,42 +3,65 @@ angular.module('app', [])
     $scope.results = results;
     $scope.jsonData = JSON.stringify(results, null, 4);
 
-    var metrics = {
-      totalSuites: results.suites.length,
-      passedSuites: 0,
-      totalSpecs: 0,
-      passedSpecs: 0,
-      totalExp: 0,
-      passedExp: 0
-    };
+    function countKPIs() {
+      var metrics = {
+        totalSuites: 0,
+        passedSuites: 0,
+        totalSpecs: 0,
+        passedSpecs: 0,
+        totalExp: 0,
+        passedExp: 0,
+        timeMin: '02',
+        timeSec: '25'
+      };
 
-    for (var i = 0; i < results.suites.length; i++) {
-      var thisSuite = results.suites[i],
-        numFailedSpecs = 0, // failed specs in this suite.
-        numPassedExp = 0;
+      function traverse(node) {
+        console.log(node.type + ' - ' + node.id );
 
-      for (var j = 0; j < thisSuite.specs.length; j++) {
-        var thisSpec = thisSuite.specs[j],
-          failedExpectations = thisSpec.failedExpectations,
-          passedExpectations = thisSpec.passedExpectations;
-
-        if (failedExpectations.length > 0) {
-          numFailedSpecs ++;
+        var isPass = true;
+        if (node.children) {
+          for (var i = 0; i <  node.children.length; i++) {
+            isPass &= traverse(node.children[i]);
+          }
         }
 
-        numPassedExp += passedExpectations.length;
+        switch (node.type) {
+          case 'suite':
+            // we are counting suites at ALL levels.
+            metrics.totalSuites ++;
+            if (isPass) {
+              metrics.passedSuites ++;
+            }
+            break;
 
-        metrics.totalExp += (passedExpectations.length + failedExpectations.length);
-        metrics.passedExp += passedExpectations.length;
+          case 'spec':
+            var spec = node;
+            isPass = (spec.failedExpectations.length == 0);
+            metrics.totalExp += (spec.failedExpectations.length + spec.passedExpectations.length);
+            metrics.passedExp += spec.passedExpectations.length;
+
+            metrics.totalSpecs ++;
+            if (isPass) {
+              metrics.passedSpecs ++;
+            }
+            break;
+
+          default:
+            break;
+        }
+
+        return isPass;
       }
 
-      metrics.totalSpecs += thisSuite.specs.length;
-      metrics.passedSpecs += (thisSuite.specs.length - numFailedSpecs);
+      traverse(results);
 
-      if (numFailedSpecs == 0) {
-        metrics.passedSuites ++;
-      }
+      // determine total time.
+      // TODO: nimmi, take the data in results.startTime & results.endTime, and calculate metrics.timeMin & metrics.timeSec
+
+      return metrics;
     }
 
-    $scope.metrics = metrics;
+    $scope.metrics = countKPIs();
+
+
   });
